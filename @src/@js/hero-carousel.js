d6813978b1717015e7724e9abebc7acd90ba1ae3
SIROFSIN <ksmirnov23@gmail.com>
2023-08-04 13:53:56 +0300

    Header_done Hero_done

---
+++ @src/@js/hero-carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.hero-section__carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,    
+    arrows: false,
+    dots: false,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear'
+    });
+});
