fb8cd20f69ce3fc1b3a759b0ec943844d1c4e746
SIROFSIN <ksmirnov23@gmail.com>
2023-08-07 11:12:26 +0300

    ms fix

--- @src/@js/manual-slider.js
+++ @src/@js/manual-slider.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-  $('#slide-btn-1').focus();
+  $(document).ready(function() {
+    // Получаем все кнопки слайдов
+    var $slideBtns = $('.slide-choose-btns-wrapper__slide-choose-btn');
+    // Получаем область в которой расположены кнопки (здесь необходимо сохранять фокус)
+    var $slideWrapper = $('.how-we-work-section__slide-choose-btns-wrapper');
+    // Индекс выбранной кнопки
+    var selectedBtnIndex = 0;
+    // Установка фокуса на первой кнопке при загрузке страницы
+    $slideBtns.eq(0).focus();
+  
+    // Функция обработки клика (с переводом фокуса)
+    $slideBtns.on('click', function() {
+      selectedBtnIndex = $slideBtns.index(this);
+      $(this).focus();
+    });
+
+    // Функция сохранения фокуса внутри заданой области (блока)
+    $slideWrapper.on('mousedown', function(event) {
+      event.preventDefault();
+    });
+
+    // Функция сохранения фокуса на последнем выбранном элементе при клике вне области (блока)
+    $(document).on('mousedown', function(event) {
+      if (!$(event.target).closest('.how-we-work-section__slide-choose-btns-wrapper').length) {
+        setTimeout(function() {
+          $slideBtns.eq(selectedBtnIndex).focus(); }, 0);
+      }
+    });
+  });
+  
 });
   $(document).ready(function manualSlider() {
       // Получаем все кнопки выбора слайдов
