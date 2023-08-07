6dbb33be80ad8bdc09fa96a277e994fd94b61d04
SIROFSIN <ksmirnov23@gmail.com>
2023-08-07 10:42:48 +0300

    manual-slider fix

--- @src/@js/manual-slider.js
+++ @src/@js/manual-slider.js
@@ -1,15 +1,18 @@
-$(document).ready(function manualSlider() {
-    $('#slide-btn-1').focus()
-    // Получаем все кнопки выбора слайдов
-    const slideButtons = $('.slide-choose-btns-wrapper__slide-choose-btn');
-    // Получаем все слайды
-    const slides = $('.manual-slider-box__slide-item');
-    // Обработчик клика на кнопки выбора слайдов
-    slideButtons.click(function slideChoose() {
-    const index = $(this).data('step');
-      // Скрываем все слайды
-    slides.hide();
-      // Отображаем выбранный слайд
-    $(slides[index]).fadeIn();
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  $('#slide-btn-1').focus();
 });
+  $(document).ready(function manualSlider() {
+      // Получаем все кнопки выбора слайдов
+      const slideButtons = $('.slide-choose-btns-wrapper__slide-choose-btn');
+      // Получаем все слайды
+      const slides = $('.manual-slider-box__slide-item');
+      // Обработчик клика на кнопки выбора слайдов
+      slideButtons.click(function slideChoose() {
+      const index = $(this).data('step');
+        // Скрываем все слайды
+      slides.hide();
+        // Отображаем выбранный слайд
+      $(slides[index]).fadeIn();
+      });
+  });
+
